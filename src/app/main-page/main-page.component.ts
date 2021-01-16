43438a217cc786bcc603ef68f4c95ddc0505b027
Igor Xavier <igoorlimaa@gmail.com>
2021-01-16 12:43:15 -0300

    modifica main-page component para mostrar o valor total das despesas de forma dinamica

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -9,6 +9,7 @@ import { BudgetItem } from '../models/budget-item.model';
 export class MainPageComponent implements OnInit {
 
   budgetItems: BudgetItem[] = new Array<BudgetItem>();
+  totalBudget: number = 0;
 
   constructor() { }
 
@@ -16,12 +17,14 @@ export class MainPageComponent implements OnInit {
   }
 
   addItem(newItem: BudgetItem) {
-    this.budgetItems.push(newItem)
+    this.budgetItems.push(newItem);
+    this.totalBudget += newItem.amount;
   }
 
   deleteItem(item: BudgetItem) {
     let index = this.budgetItems.indexOf(item);
     this.budgetItems.splice(index, 1);
+    this.totalBudget -= item.amount;
   }
 
 }
