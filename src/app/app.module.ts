d9b3439276fb1a5febed71ed7857c8c6f30e2b5e
Igor Xavier <igoorlimaa@gmail.com>
2021-01-16 11:20:20 -0300

    adiciona componentes item-list e item-card + styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { ItemCardComponent } from './item-list/item-card/item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    AddItemFormComponent
+    AddItemFormComponent,
+    ItemListComponent,
+    ItemCardComponent
   ],
   imports: [
     BrowserModule,
